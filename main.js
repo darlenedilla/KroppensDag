f0561ee1c3b895108c9bb3342bee04449c53770e
darlenedilla <darlenedilla@hotmail.com>
2018-12-14 11:12:03 +0100

    Fjernet kommentarer  fra JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var interval;
 function countDown(){
     // Set the date we're counting down to
     var countDownDate = new Date("Apr 5, 2019 10:00:00").getTime();
+    console.log(countDownDate);
 
         // Get todays date and time
         var now = new Date().getTime();
@@ -35,15 +36,19 @@ interval = setInterval(countDown,1000);
 
 function toggleCountDown(){
     var countdownElement = document.getElementById("countdownWrapper");
-            
+    
+    //Tilføjer event listener på hver elemenet med id="countdownWrapper"
     countdownElement.addEventListener("click", function(){
 
+        //Tjekker om countdownElement har class="activated"
         if(countdownElement.classList.contains("activated")){
+
+            //Kalder setInterval for at starte countdown igen
             interval = setInterval(countDown,1000);
             countdownElement.classList.remove("activated");
-            console.log("I was clicked");
         }
         else{
+            //Kalder clearInterval for at stoppe countdown
             clearInterval(interval);
             countdownElement.innerHTML = "5. APRIL 2019 KL 10-17";
             countdownElement.classList.add("activated");
@@ -61,11 +66,11 @@ toggleCountDown();
     
 var acc = document.getElementsByClassName("accordion"); // Der bliver lavet en variabel der hedder acc. Variablens værdi er alle de klasser som hedder accordion. Den kalder på alle elementerne i html der hedder accordion
  
-for (i = 0; i < acc.length; i++) {   // For loop. Array = acc. Her løber den hele Arrayet igennem. Length = hvor mange elementer der er. Denne forklarer, at den ligger en til i hvert loop. Dvs den tager automatisk det næste array, istedet for at man skal skrive det hundrede gange. 
-  acc[i].addEventListener("click", function() { // Array. For hvert element i det rray der hedder acc, tilføjes der en eventlistener som hedder klik. Denne eventlistener har en funktion, som kommer fra næste linje.  
-    this.classList.toggle("active"); // hver gang vi klikker, skal den have active og ikke active. Toggle betyder active/ikke active
-    var panel = this.nextElementSibling; // panel er "sibling" element. I html er "panel" lige efter "accordion" og derfor er det sibling element. 
-    if (panel.style.maxHeight){ // Her tjekkes om den styling på  panel der hedder hedder 'max-height', har en værdi. Hvis den har en værdi, sætter den den til null. Hvis den ikke har en værdi, gården ud fra hvor meget tekst der står. 
+for (i = 0; i < acc.length; i++) {     
+  acc[i].addEventListener("click", function() {  
+    this.classList.toggle("active"); //Toggler mellem at tilføje og fjerne class'en "active"
+    var panel = this.nextElementSibling;  
+    if (panel.style.maxHeight){ 
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px"; 
@@ -86,27 +91,27 @@ var menubtn = document.getElementsByClassName("menubtn");
 var closeBtn = document.getElementsByClassName("close");
 
 // When click, open the modal
-    for(i = 0; i < menubtn.length; i++){ // For-loop, der gennemgår array'et med alle menuknapperne
+    for(i = 0; i < menubtn.length; i++){ 
         menubtn[i].onclick = function(clickedBtn) { // Tilføjer en eventlistener (click) til hver menuknap
-            if(clickedBtn.target == document.getElementById("hvhvhvBtn")){ //Her tjekkes om den knap, der trykkes på har ID 'hvhvhvBtn'
-                lightbox1.style.display = "block"; // Viser lightbox 1
-                lightbox2.style.display = "none"; // Skjuler lightbox 2
-                lightbox3.style.display = "none"; //Skjuler lightbox 3
+            if(clickedBtn.target == document.getElementById("hvhvhvBtn")){ 
+                lightbox1.style.display = "block"; 
+                lightbox2.style.display = "none";
+                lightbox3.style.display = "none"; 
             }
             if(clickedBtn.target == document.getElementById("programBtn")){ //Her tjekkes om den knap, der trykkes på har ID 'programBtn'
-                lightbox2.style.display = "block"; // Viser lightbox 2
-                lightbox1.style.display = "none"; //Skjuler lightbox 1
-                lightbox3.style.display = "none"; //Skjuler lightbox 3
+                lightbox2.style.display = "block"; 
+                lightbox1.style.display = "none"; 
+                lightbox3.style.display = "none"; 
             }
-            if(clickedBtn.target == document.getElementById("contactBtn")){ //Her tjekkes om den knap, der trykkes på har ID 'contactBtn'
-                lightbox3.style.display = "block"; // Viser lightbox 3
-                lightbox1.style.display = "none"; // Skjuler lightbox 1
-                lightbox2.style.display = "none"; // Viser lightbox 2
+            if(clickedBtn.target == document.getElementById("contactBtn")){ 
+                lightbox3.style.display = "block"; 
+                lightbox1.style.display = "none"; 
+                lightbox2.style.display = "none"; 
             }
         }
     };
 
-    for(i = 0; i < closeBtn.length; i++){ // For-loop, der gennemgår array'et med X-knapperne i lightboxen
+    for(i = 0; i < closeBtn.length; i++){ 
         closeBtn[i].onclick = function() { // Tilføjer en eventlistener (click) til hver X-knap
             //Herunder skjules alle lightboxene
             lightbox1.style.display = "none";
